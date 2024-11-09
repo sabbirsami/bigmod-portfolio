377465f5d0be6adb26160f324d158ef8cb28cbcc
Sabbir Mohammad Sami <smd71430@gmail.com>
2024-11-09 16:13:29 +0600

    create shared Title components

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,12 @@
+import Title from "@/components/shared/title/Title";
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="">
+      <Title title="About Us" url="/about-us" />
+    </section>
+  );
+};
+
+export default page;
---
+++ src/components/shared/title/Title.tsx
@@ -0,0 +1,49 @@
+import titleBg from "@/assets/bg/dot_background.png";
+import Link from "next/link";
+import TitleArrow from "../icons/TitleArrow";
+
+type TitleType = {
+  title: string;
+  url: string;
+};
+
+const Title = ({ title, url }: TitleType) => {
+  return (
+    <section
+      className=""
+      style={{
+        background: `url('${titleBg.src}')`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100%",
+      }}
+    >
+      <div className=" container mx-auto py-32">
+        <h3
+          className="uppercase text-7xl font-semibold"
+          style={{
+            WebkitTextStroke: "1px #B473F3",
+            WebkitTextFillColor: "transparent",
+            color: "transparent",
+          }}
+        >
+          {title}
+        </h3>
+        <div className="flex items-center py-4 gap-6">
+          <p className="">
+            <Link href={"/"}>Home</Link>
+          </p>
+          <TitleArrow />
+          <p className="">
+            <Link href={url} className="capitalize text-secondary_two">
+              {title}
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Title;
